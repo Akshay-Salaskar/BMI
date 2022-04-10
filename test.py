78ed24b508861b67496cc27a8e201f68c428c774
Akshay-Salaskar <103364109+Akshay-Salaskar@users.noreply.github.com>
2022-04-10 23:41:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ import pandas as pd
 import pytest
 
 
-def test_some_func(x):
+def test_bmi(x):
     a = x["WeightKg"]/(x["HeightCm"]/100)
     b = "Underweight" if a < 18.4 else "Normal weight" if a >= 18.5 and a <= 24.9 else  "Overweight" if a >= 25 and a <= 29.9 else "Moderately obese" if a >=30 and a <= 34.9 else "Severely obese" if a >= 35 and a <= 39.9 else "Very severely obese"
     c = "Malnutrition risk" if b == "Underweight"  else "Low risk" if b == "Normal weight" else "Enhanced risk" if b == "Overweight" else "Medium risk" if b == "Moderately obese" else "High risk" if b == "Severely obese" else "Very high risk"
     return a,b,c
 def test_bmi_calculator(df):
-    df[['BMI','BMI Cat','Health risk']] = df.apply(some_func,axis=1, result_type ='expand')
+    df[['BMI','BMI Cat','Health risk']] = df.apply(test_bmi,axis=1, result_type ='expand')
     df.to_json("sample2.json", orient = 'records', index = 'false')
 
 def test_count_weight(col_name):
